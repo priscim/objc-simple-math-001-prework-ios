25055af5648993b14ead4cf9171f67e625bc3e80
priscim <priscim@aol.com>
2016-03-31 17:29:38 -0400

    Done.

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -9,6 +9,34 @@
     
     /**
      
+// NSInteger=i
+// i=1+1
+// NSLog(@"%li");
+// NSLog(@"i:%li",i);
+     
+// *lu=2
+// NSLog(@"*lu");
+     
+// NSUInteger=a+b
+// a=2
+// b=3
+     
+   %li=a
+//  a=17
+//   %li=b
+//     b=29
+//     %li=c
+//     c=a-b
+//     NSLog(@"a:%li,b:%li,c:%li",a,b,c);
+     
+//  NSUInteger=u
+//     u=8-10
+//     NSLog(@"u:%lu",u);
+  
+     BOOL threeisequaltothree = 3 == 3
+     NSLog (@"3 == 3:%d", threeisequaltothree);
+     
+     
      * Write your code here.
      
      */
